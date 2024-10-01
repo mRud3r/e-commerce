50403e95f581b12c529bf6b09967e1749235b1b0
mRud3r <mrud.priv@gmail.com>
2024-10-01 23:09:18 +0200

    added Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Product from './components/Product.jsx'
 import productsData from './dummyProducts.json'
+import Navigation from './components/Navigation.jsx'
 
 
 function App() {
 
   return (
     <>
+    <Navigation />
     <main className='p-6 flex gap-2'>
     {productsData.products.map((product) => (
       <Product name={product.name} price={product.price} />
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+export default function Navigation() {
+    return (
+        <nav className="w-full fixed top-0 bg-slate-500">
+            <ul className="flex gap-4 p-3 justify-center">
+                <li>Home</li>
+                <li>Products</li>
+                <li>Category</li>
+                <li>About Us</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
