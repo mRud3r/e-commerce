d82af78d07350100aca370baaeac7293899c890d
mRud3r <mrud.priv@gmail.com>
2024-10-06 17:03:10 +0200

    styled navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,27 +9,26 @@ export default function Navigation() {
     const [visible, setVisible] = useState(false);
 
 	return (
-		<nav className="w-full fixed top-0 text-stone-900 flex flex-row items-center justify-between px-8">
-			<h1 className="text-2xl font-thin">+ The Router Side +</h1>
-			<ul className="hidden sm:flex gap-4 p-3 justify-center">
+		<nav className="w-full h-14 fixed px-4 top-0 flex flex-row items-center justify-between">
+			<div className="flex flex-row items-center">
+			<a href="/" className="text-lg font-bold">+ Router Side +</a>
+			<ul className="hidden sm:flex gap-4 px-4 justify-center text-stone-600">
 				<li>
-					<Link to="/">Home</Link>
+					<Link to="/" className="hover:text-stone-900" >Home</Link>
 				</li>
 				<li>
-					<Link to="/collection">Collection</Link>
+					<Link to="/collection" className="hover:text-stone-900">Collection</Link>
 				</li>
 			</ul>
+			</div>
 			<div className="flex flex-row items-center gap-4">
-				<div>
-                <PiShoppingCartSimpleThin className="w-7 h-7 cursor-pointer" />
-					<p className="absolute bottom-[-2px]">10</p>
-				</div>
+                <PiShoppingCartSimpleThin className="size-8 cursor-pointer" />
                 <CiMenuFries onClick={() => setVisible(true)} className="w-7 h-7 cursor-pointer sm:hidden" />
 			</div>
 
             {/* Mobile Sidebar Menu */}
-            <div className={`absolute top-0 left-0 bottom-0 overflow-hidden bg-slate-50 ${visible ? 'w-full' : 'w-0'}`}>
-            <TfiClose onClick={() => setVisible(false)} />
+            <div className={`fixed top-0 left-0 bottom-0 overflow-hidden transition-all bg-slate-50 ${visible ? 'w-full' : 'w-0'}`}>
+            <TfiClose onClick={() => setVisible(false)} className="absolute top-4 right-4" />
             </div>
 
 		</nav>
