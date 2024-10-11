f491b3087a247668c1805ed10d34173a04ce3a7c
mRud3r <166493649+mRud3r@users.noreply.github.com>
2024-10-11 23:15:27 +0200

    small improvements

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -6,16 +6,24 @@ import CartItem from "./CartItem";
 export default function CartContent( {cartVisible, setCartVisible} ) {
 const {cartItems} = useContext(CartContext);
 
+const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
     return (
         <div className={`fixed top-0 right-0 bottom-0 overflow-hidden transition-all bg-stone-50 ${cartVisible ? 'w-96' : 'w-0'}`}>
             <TfiClose onClick={() => setCartVisible(false)} className="absolute top-4 right-4" />
-            <h1 className="text-3xl mt-8 ml-8">Cart</h1>
-            <div className="flex flex-col mt-8 items-center justify-center">
-                <p className="text-sm">Your Cart is Empty</p>
-            </div>
-            {cartItems.map(item => (
+            <h1 className="text-3xl mt-8 ml-8">Cart({totalItems})</h1>
+            
+            {totalItems === 0 ? (
+                    <div className="py-8 flex items-center justify-center">
+                        <p className="text-sm">Your Cart is Empty</p>
+                    </div>
+            
+        ) : (
+            cartItems.map(item => (
                 <CartItem key={item.id} item={item} />
-            ))}
+            ))
+        )}
+
         </div>
     );
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,9 +3,9 @@ import ContactForm from "../components/ContactForm";
 export default function ContactPage() {
     return (
             <main className="container mx-auto p-4 min-h-screen flex flex-row justify-center items-center gap-4">
-            <div className="w-full">
+            <div className="w-full flex flex-col gap-4">
                 <h1 className="text-2xl">Contact Us</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam officiis hic nihil corporis ut deserunt aut impedit suscipit tempora, alias, aliquam iste quas sit rem eveniet, minus sint! Laudantium, eaque!</p>
+                <p className="font-light text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam officiis hic nihil corporis ut deserunt aut impedit suscipit tempora, alias, aliquam iste quas sit rem eveniet, minus sint! Laudantium, eaque!</p>
 
             </div>
             <ContactForm />
