94b252e887507c56d79e5400dceb6d6b72269848
mRud3r <166493649+mRud3r@users.noreply.github.com>
2024-10-11 22:38:37 +0200

    added Form functionality

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,15 +1,50 @@
 import { useState } from "react"
 
 export default function ContactForm() {
-    const [name, setName] = useState('');
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        setFormData({
+            name: '',
+            email: '',
+            message: ''
+        })
+    }
 
     return (
 
-        <form>
-            <label className="flex flex-col gap-1 text-sm">
-                Name
-                <input type="text" onChange={(e) => setName(e.target.value)} value={name} placeholder='name' className="bg-stone-300 h-20 w-60 p-2" />
-            </label>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full">
+            
+            <div>
+            <label className="flex flex-col gap-1 text-sm">Name</label>
+            <input type="text" onChange={handleChange} name="name" value={formData.name} placeholder='name' className="bg-stone-200 w-full p-4" />
+            </div>
+
+            <div className="flex flex-col gap-1 text-sm">
+            <label>E-mail</label>
+            <input type="email" onChange={handleChange} name="email" value={formData.email} placeholder='email' className="bg-stone-200 w-full p-4" />
+            </div>
+
+            <div className="flex flex-col gap-1 text-sm">
+            <label>Message</label>
+            <textarea name="message" value={formData.message} onChange={handleChange} placeholder="your message..." className="bg-stone-200 w-full p-4"></textarea>
+            </div>
+
+            <button type="submit" className="bg-stone-700 hover:bg-stone-800 text-stone-50 py-4" >Send Message</button>
         </form>
     );
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,10 +2,13 @@ import ContactForm from "../components/ContactForm";
 
 export default function ContactPage() {
     return (
-        <>
-        <main className="min-h-screen flex items-center justify-center">
+            <main className="container mx-auto p-4 min-h-screen flex flex-row justify-center items-center gap-4">
+            <div className="w-full">
+                <h1 className="text-2xl">Contact Us</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam officiis hic nihil corporis ut deserunt aut impedit suscipit tempora, alias, aliquam iste quas sit rem eveniet, minus sint! Laudantium, eaque!</p>
+
+            </div>
             <ContactForm />
         </main>
-        </>
     );
 }
